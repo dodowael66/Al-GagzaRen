d8b38a5022281f593a0505d003d51fa9222a983e
dodowael66 <57775220+dodowael66@users.noreply.github.com>
2019-11-15 08:51:13 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -43,9 +43,9 @@ client.on("message", async message => {
                                 m.delete();
                                     message.channel.send().then(msg => msg.delete(5000));
                                         message.guild.members.forEach(member => {
+                                            let bc = new Discord.RichEmbed()
                                             .addField("Message", args);
 
-                                            member.sendEmbed(bc);
                                         });
                         });
                         no.on("collect", v => {
