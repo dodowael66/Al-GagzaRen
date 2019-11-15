19141eb5a25e8a2f311878c441715117446f937e
dodowael66 <57775220+dodowael66@users.noreply.github.com>
2019-11-15 08:43:11 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -78,14 +78,8 @@ client.on("message", async message => {
                                     message.channel.send(`:ballot_box_with_check: | Done ... The Broadcast Message Has Been Sent For ${message.guild.members.filter(r => r.presence.status !== "offline").size} Members`).then(msg => msg.delete(5000));
                                         message.guild.members.filter(r => r.presence.status !== "offline").forEach(member => {
                                             let bco = new Discord.RichEmbed()
-                                            .setColor("RANDOM")
-                                            .setThumbnail(message.author.avatarURL)
-                                            .setTitle("Broadcast")
-                                            .addField("Server", message.guild.name)
-                                            .addField("Sender", message.author.username)
                                             .addField("Message", args);
 
-                                            member.sendEmbed(bco);
                                         });
                         });
                         no.on("collect", v => {
